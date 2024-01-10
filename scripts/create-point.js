25e92f5b9c5f68a5779a95a052a436bc9d2b5ee5
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-01-10 01:25:54 -0300

    handleSelectedItem functon for the create-point page form

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -47,3 +47,37 @@ document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities);
 
+
+// ITENS DE COLETA
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for( const item of itemsToCollect ){
+  item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event){
+  const itemLi = event.target
+  itemLi.classList.toggle('selected')
+
+  const itemId = itemLi.dataset.id
+
+  const alreadySelected = selectedItems.findIndex( item => {
+     return item == itemId
+    
+  })
+
+  if( alreadySelected >= 0){
+    const filteredItems = selectedItems.filter( item => {
+      const itemIsDifferent = item != itemId
+      return itemIsDifferent
+    })
+    selectedItems = filteredItems
+  }else{
+    selectedItems.push(itemId)
+  }
+  collectedItems.value = selectedItems
+}
