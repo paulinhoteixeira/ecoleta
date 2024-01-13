27012f5ad7265f80f08f2e51b2ec098b81312519
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-01-13 00:41:35 -0300

    partials/modal.html added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,6 @@
 const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
-const close = document.querySelector("#moda .header a")
+const close = document.querySelector("#modal .header a")
 
 buttonSearch.addEventListener("click", () => {
   modal.classList.remove("hide")
--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const server = express()
 
+//CONFIGURAR PASTA PUBLICA
 server.use(express.static("public"))
 
+// UTILIZANDO TEMPLATE ENGINE
 const nunjucks = require('nunjucks')
 nunjucks.configure("src/views", {
   express: server,
   noCache: true
 })
 
+
+// CONFIGURAR CAMINHOS DA APLICAÇÃO
+// PÁGINA INICIAL
+// REQ: REQUISIÇÃO
+// RES: RESPOSTA
 server.get('/', (req, res) => {
-  return res.render("index.html", { title: "Um título"})  
+  return res.render("index.html")  
 })
 server.get('/create-point', (req, res) => {
   return res.render("create-point.html")  
@@ -19,4 +26,5 @@ server.get('/search', (req, res) => {
   return res.render("search-results.html")  
 })
 
+// LIGAR O SERVIDOR
 server.listen(3000)
